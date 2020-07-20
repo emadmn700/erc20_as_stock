1298a679de1bc23df7db21c486f67e784349f6c5
emadmn700 <68559920+emadmn700@users.noreply.github.com>
2020-07-21 04:22:03 +0430

    Update erc20 as stock.sol

--- erc20 as stock.sol
+++ erc20 as stock.sol
@@ -8,15 +8,18 @@ contract ERC20_AS_STOCK {
     // a 2D array which in every profit distribution time matches addresses to their state of withdrawal
     mapping (uint64 =>  mapping (address => bool) ) withdrawn;      
     
-    // i don't think anyone would listen to this event but it would be more 'standard' so i put it
+    // contract events (i wonder if anyone listens to them... just maybe crypto news sites)
     event ProfitReceived(address indexed receiver,uint amount);
+    event WithdrawalAllowed(uint profit_amount);
+    event WithdrawalDisallowed(uint remaining_amount);
     
-    bool withdrawal_allowed;
-    address owner;
-    uint256 profit;
+    bool public withdrawal_allowed;
+    address public owner;
+    uint256 public profit;
     // the below one is because resetting the withdrawn-refrence is not possible and you have to reset one-by-one
     // so it would be better to put a count or nonce so the previous data would be abandoned
-    uint64 distribution_count = 0;
+    // also the owner can access previous data
+    uint64 public distribution_count = 0;
 
 
     string public name = "MyCompany";       // token name
@@ -42,43 +45,46 @@ contract ERC20_AS_STOCK {
     }
 
     function () payable external {
-        // if the sender is the owner it means he/she is gonna either distribute the profit (note that the profit is in ETH) or finish the withdrawal time
-        // I know it might seem stupid but as i decided if the owner sends 0 ETH it means he/she is gonna end the profit withdrawal time
-        // And if some amount of ETH is sent by the owner it's the profit which is gonna be distributed
-        // the owner has to change the gas limit to at least 60,000 else will face error
-        if(msg.sender == owner && msg.value == 0){
-            require( withdrawal_allowed , "You first have to start the proccess of distributing profit then you may end it" );
-            withdrawal_allowed = false;
-            distribution_count++;
-        }
-        if(msg.sender == owner && msg.value != 0){
-            require( !withdrawal_allowed , "You first have to end the corrent profit distribution proccess then you can start a new one" );
-            withdrawal_allowed = true;
-            // in my idea the profit whih is gonna be distributed is not only the value sent by the owner but the whole contract ETH balance
-            profit = address(this).balance;  
-        }
+        require( msg.sender != owner , "what the hell are you doing manager?");
         // stock owners have to send a transaction to the contract ( 0 ETH value ofcourse , just the fee will apply for them ) to withdraw their profit
         // note that it's diffrent from token transferring and they have to send (0 ofcourse) ETH to the contract address
-        // the point is that the stock owners who's gonna withdraw their profit have to change the gas limit ( 70,000 is enough )
+        // the point is that the stock owners who's gonna withdraw their profit have to change the gas limit ( i'll update the needed gas soon )
         // the gas limit for ETH transaction is default set to 21000 but it's not enough as some proccess is happening here
         // Sorry for this structure but i think it was best...
         // changing gas limit and sending an amount is much easier for usual people rather than calling a function of the contract 
-        if(msg.sender != owner){
-            require( !withdrawn[distribution_count][msg.sender] , "You've already withdrawn your profit" );
-            require( withdrawal_allowed , "withdrawal time has been finished" );
-            withdrawn[distribution_count][msg.sender] = true;
-            
-            uint256 profit_amount = balances[msg.sender]*profit / ( totalSupply - balances[owner] );
-            // address-balance devided by stock-owners-supply is the fraction of every one in the company so they will receive that fraction of profit
-            // solidity is not a friend of decimal nums so i've got first write balance*profit then deviding it by stock-owners-supply
-            // note that the profit value is in wei ( 1e-18 ETH )
-            msg.sender.transfer( profit_amount );
-            emit ProfitReceived(msg.sender , profit_amount);
-        }
-        // didn't use 'else' so it would be easier to understand, if your'e gonna use this code, do it!
+        
+        require( !withdrawn[distribution_count][msg.sender] , "You've already withdrawn your profit" );
+        require( withdrawal_allowed , "withdrawal time has been finished" );
+        withdrawn[distribution_count][msg.sender] = true;
+
+        uint256 amount = balances[msg.sender]*profit / ( totalSupply - balances[owner] );
+        // address-balance devided by stock-owners-supply is the fraction of every one in the company so they will receive that fraction of profit
+        // solidity is not a friend of decimal nums so i've got first write balance*profit then deviding it by stock-owners-supply
+        // note that the profit value is in wei ( 1e-18 ETH )
+        msg.sender.transfer( amount );
+        emit ProfitReceived(msg.sender , amount);
     }
     
+    // the owner has to send the profit in ETH to this function(note that he's not gonna send his own fraction or part)
+    function AllowWithdrawal() payable external{
+        require( msg.sender == owner , "so what do you think you're doing?");
+        require( !withdrawal_allowed , "You first have to end the corrent profit distribution proccess then you can start a new one" );
+        withdrawal_allowed = true;
+        // the profit whih is gonna be distributed is the whole contract ETH balance
+        profit = address(this).balance;
+        emit WithdrawalAllowed(profit);
+    }
     
+    // just sending 0 ETH to this function will end withdrawal time and send back the remaining value to the owner
+    function DisallowWithdrawal() payable external{
+        require( msg.sender == owner , "so what do you think you're doing?");
+        require( withdrawal_allowed , "You first have to start the proccess of distributing profit then you may end it" );
+        withdrawal_allowed = false;
+        distribution_count++;
+        uint256 remaining = address(this).balance;
+        msg.sender.transfer( remaining );                // the profit of those who didn't withdraw will sent back to the owner :)
+        emit WithdrawalDisallowed(remaining);
+    }
     
     // The below functions are no matter for us, just for respecting the ERC20 standard
 
