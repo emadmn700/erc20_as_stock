1ec3c5e5200485d682e3ce4fe146523cbd2c0c31
Mahdi Noor <68559920+emadmn700@users.noreply.github.com>
2020-08-10 19:36:26 +0430

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -2,37 +2,42 @@ pragma solidity ^0.5.16;
 
 contract ERC20_AS_STOCK {
 
-    mapping (address => uint256) public balances;                   
+    mapping (address => uint256) public balances;      
+    mapping (uint32 =>  mapping (address => bool) ) public withdrawn;       // uint32 : distribution_count , bool : profit received or not
+    mapping (uint32 =>  mapping (address => bool) ) public voted;           // uint32 : election_count , bool : voted or not
+    mapping (uint32 =>  Proposal[] ) public proposals;                      // uint32 : election_count
     
-    // a 2D array which in every profit distribution time matches addresses to their state of withdrawal
-    mapping (uint32 =>  mapping (address => bool) ) withdrawn;
+    struct Proposal { string title; uint256 vote_count; }
     
-    // i wonder if anyone listens to these...
     event WithdrawalAllowed(uint profit_amount);
     event WithdrawalEnded(uint remaining_amount);
     
-    bool public withdrawal_allowed;
     address public owner;
     uint256 public profit;
-    // the below one is because resetting the withdrawn-refrence is not possible and you have to reset one-by-one
-    // so it would be better to put a count or nonce so the previous data would be abandoned
-    // also the owner can access previous data
-    uint32 public distribution_count = 0;
-
+    bool public withdrawal_allowed;
+    bool public voting_allowed;
+    uint32 public distribution_count = 0;           // every time the company distributes its profit
+    uint32 public election_count = 0;               // every time the company runs an election proccess
 
     string public name = "MyCompany";       // token name
     uint8 public decimals = 0;              // maximum decimals it accepts
     string public symbol = "SCS";           // sample company stock
     uint public totalSupply = 10**9;        // One billion stock papers 
 
+    modifier onlyOwner {
+        require( msg.sender == owner, "Only owner can call this function." );
+        _;
+    }
+
     constructor() public {
         owner = msg.sender;
-        balances[msg.sender] = totalSupply;                  // gives the creator all initial tokens
+        balances[msg.sender] = totalSupply;
     }
 
     function transfer(address _to, uint256 _value) external returns (bool success) {
-        // it would be a great bug if any transfer happens during withdrawal time
-        require( !withdrawal_allowed , "Sorry but you can't transfer during profit distribution time");
+        
+        require( !withdrawn[distribution_count][msg.sender] , "As you've received the company stock profit, you can't transfer untill withdrawal time is ended");
+        require( !voted[election_count][msg.sender] , "As you have voted, you can't transfer untill voting time is ended");
         
         require(balances[msg.sender] >= _value);
         balances[msg.sender] -= _value;
@@ -54,14 +59,13 @@ contract ERC20_AS_STOCK {
     }
     
     // SECOND WAY : stock owners can send a transaction (with 0 amount of ETH ofcourse) to the contract address for withdrawal, just the fee will apply
-    // note that they have to change the gas limit (70K is enough)
+    // note that they have to change the gas limit (60K is enough)
     function () payable external {
         ReceiveYourProfit();
     }
     
-    // the owner has to send the profit in ETH to this function(note that he's not gonna send his own fraction)
-    function AllowWithdrawal() payable external{
-        require( msg.sender == owner , "so what do you think you're doing?");
+    // The owner has to send the profit in ETH to this function(note that he's not gonna send his own fraction)
+    function AllowWithdrawal() payable external onlyOwner{
         require( !withdrawal_allowed , "You first have to end the current profit distribution proccess then you can start a new one" );
         withdrawal_allowed = true;
         // the profit whih is gonna be distributed is the whole contract ETH balance
@@ -69,9 +73,7 @@ contract ERC20_AS_STOCK {
         emit WithdrawalAllowed(profit);
     }
     
-    // calling this function will end withdrawal time and send back the remaining value to the owner
-    function EndWithdrawal() external{
-        require( msg.sender == owner , "so what do you think you're doing?");
+    function EndWithdrawal() external onlyOwner{
         require( withdrawal_allowed , "You first have to start the proccess of distributing profit then you may end it" );
         withdrawal_allowed = false;
         distribution_count++;
@@ -80,7 +82,38 @@ contract ERC20_AS_STOCK {
         emit WithdrawalEnded(remaining);
     }
     
+    function StartElection(string[] _titles) external onlyOwner{
+        require( !voting_allowed , "You first have to end the current election then you can start a new one" );
+        voting_allowed = true;
+        
+        for (uint i = 0; i < _titles.length; i++) {
+            proposals[election_count].push( Proposal( { title: _titles[i] , vote_count: 0 } ) );
+        }
+    }
     
+    function EndElection() external onlyOwner returns(string winner_title){
+        require( voting_allowed , "You first have to start an election then you may end it" );
+        voting_allowed = false;
+        
+        uint winningVoteCount = 0;
+        for (uint p = 0; p < proposals[election_count].length; p++) {
+            if (proposals[election_count][p].vote_count > winningVoteCount) {
+                winningVoteCount = proposals[election_count][p].vote_count;
+                winner_title = proposals[election_count][p].title;
+            }
+        }
+        
+        election_count++;
+    }
+    
+    function Vote(uint _id) external{
+        require( !voted[election_count][msg.sender] , "Changing the vote is not possible" );
+        require( voting_allowed , "the voting period has been finished" );
+        voted[election_count][msg.sender] = true;
+        
+        // The weight of everyone in voting is their balance
+        proposals[election_count][_id].vote_count += balances[msg.sender];
+    } 
     
     // The below functions are no matter for us, just for respecting the ERC20 standard
 
@@ -90,8 +123,10 @@ contract ERC20_AS_STOCK {
     event Transfer(address indexed from, address indexed to,uint tokens);
      
     function transferFrom(address _from, address _to, uint256 _value) external returns (bool success) {
-        // it would be a great bug if any transfer happens during withdrawal time
-        require( !withdrawal_allowed , "Sorry but you can't transfer during profit distribution time");
+        
+        require( !withdrawn[distribution_count][_from] ,
+        "You can't transfer from an address which has received the company stock profit untill withdrawal time is ended");
+        require( !voted[election_count][_from] , "You can't transfer from an address which has voted untill the voting time is ended");
         
         uint256 allowance = allowed[_from][msg.sender];
         require(balances[_from] >= _value && allowance >= _value);
